cd7da26524ef6e365314b005b139d056aab4a8ec
Luis Antes <edy.1305@hotmail.com>
2022-10-14 10:45:54 -0600

    Update requested-requests-module page

--- peticiones-web/src/app/app-routing/app-routing.module.ts
+++ peticiones-web/src/app/app-routing/app-routing.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AdminBranchAbcComponent } from '../Admin/pages/branch-abc/admin-branch-abc.component';
@@ -8,7 +7,6 @@ import { AdminTypeOfProblemAbcComponent } from '../Admin/pages/type-of-problem-a
 import { AdminUserAbcComponent } from '../Admin/pages/user-abc/admin-user-abc.component';
 import { AdminUserByProblemTypeAbcComponent } from '../Admin/pages/user-by-problem-type-abc/admin-user-by-problem-type-abc.component';
 import { AdminEmployeeAbcComponent } from '../Admin/pages/employee-abc/admin-employee-abc.component';
-
 
 const routes: Routes = [
   { path: 'BranchAbc', component: AdminBranchAbcComponent},

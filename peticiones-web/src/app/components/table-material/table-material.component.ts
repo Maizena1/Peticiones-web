a009409b8754fdd6ac1d426b00999210cd0d8d07
SauloRLM <86893484+SauloRLM@users.noreply.github.com>
2022-10-14 21:19:35 -0600

    table material module solver in process

--- peticiones-web/src/app/components/table-material/table-material.component.ts
+++ peticiones-web/src/app/components/table-material/table-material.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit,ViewChild } from '@angular/core';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatTableDataSource} from '@angular/material/table';
 
 @Component({
   selector: 'app-table-material',
@@ -11,5 +13,27 @@ export class TableMaterialComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  
+  displayedColumns: string[] = ['material', 'cantidad', 'unidad', 'precio'];
+  dataSource = new MatTableDataSource<RequerimentProblem>(ELEMENT_DATA);
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
 
 }
+
+export interface RequerimentProblem {
+  id_problema: number;
+  id_requisito: number; 
+  name: string;
+  unidad: string;
+  cantidad: number;
+  precio: number;
+}
+
+const ELEMENT_DATA: RequerimentProblem[] = [
+  { id_problema: 1,id_requisito: 1,name: 'Focos', unidad: 'Pieza', cantidad: 2, precio: 40.00},  
+];

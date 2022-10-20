8ae656a7529793f9eadb3d965df7b584edf7d65d
Luis Antes <edy.1305@hotmail.com>
2022-10-20 11:41:49 -0600

    Update width of select and input components

--- peticiones-web/src/app/components/table/table.component.ts
+++ peticiones-web/src/app/components/table/table.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,ViewChild, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
 
 export interface PeriodicElement {
   name: string;
@@ -10,8 +13,10 @@ export interface PeriodicElement {
 const ELEMENT_DATA: PeriodicElement[] = [
   {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
   {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 4, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 5, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 6, name: 'Helium', weight: 4.0026, symbol: 'He'},
 ];
 
 @Component({
@@ -20,13 +25,17 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ['./table.component.css']
 })
 export class TableComponent implements OnInit {
-
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
+
+  @ViewChild(MatSort, {static: true}) sort!: MatSort;
+  @ViewChild(MatPaginator, { static: true }) paginator!: MatPaginator;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this. paginator;
   }
 
 }

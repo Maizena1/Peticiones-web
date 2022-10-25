197a656345f232c40de3f1f537d5c55e47d3cc58
Luis Antes <edy.1305@hotmail.com>
2022-10-25 12:43:24 -0600

    Fix paginator

--- peticiones-web/src/app/components/request-table/request-table.component.ts
+++ peticiones-web/src/app/components/request-table/request-table.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, OnInit, ViewChild } from '@angular/core';
-import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
-import {MatTableDataSource} from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import  { MatTableDataSource } from '@angular/material/table';
 import { request_table } from '../services/request-table';
 
 @Component({
@@ -12,30 +12,32 @@ import { request_table } from '../services/request-table';
 
 export class RequestTableComponent implements OnInit {
 
-@Input() Items: request_table[] = [];
-@Input() name_colum: string[] = [];
+  @Input() Items: request_table[] = [];
+  @Input() name_colum: String[] = [];
 
-displayedColumns: String[] = ['col1','col2','col3','col4'];
-dataSource = new MatTableDataSource(this.Items);
-nameColum: string [] = [];
-  
-@ViewChild(MatSort, {static: true}) sort!: MatSort;
-@ViewChild(MatPaginator, {static: true}) paginator!: MatPaginator;
+  displayedColumns: String[] = ['col1','col2','col3','col4'];
+  dataSource = new MatTableDataSource(this.Items);
+  nameColum: String [] = [];
+
+  @ViewChild(MatSort, {static: true}) sort!: MatSort;
+  @ViewChild(MatPaginator, {static: true}) paginator!: MatPaginator;
+
     
-constructor() { }
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 
-applyFilter(event: Event) {
-  const filterValue = (event.target as HTMLInputElement).value;
-  this.dataSource.filter = filterValue.trim().toLowerCase();
-}
 
-ngOnInit(): void {  
-    this.sort = this.sort;
-    this.paginator = this.paginator;
-    console.table(this.name_colum);
+  constructor() { }
+
+  ngOnInit () {  
+
     this.nameColum = this.name_colum;
-    console.table(this.Items);    
-    this.dataSource = new MatTableDataSource(this.Items);    
-}
+    this.dataSource = new MatTableDataSource(this.Items); 
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+            
+  }
 
 }

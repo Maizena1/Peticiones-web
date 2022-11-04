e0e33ad4049a5f0ccdd9ec57204b9a5420745838
Luis Antes <edy.1305@hotmail.com>
2022-11-04 11:41:44 -0700

    Update admin-user.ts

--- peticiones-web/src/app/Admin/pages/user-abc/admin-user-abc.component.ts
+++ peticiones-web/src/app/Admin/pages/user-abc/admin-user-abc.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Item } from '../../services/type';
+import { user, response } from '../../services/type';
+import { request_table } from 'src/app/components/services/request-table';
 
 @Component({
   selector: 'app-admin-user-abc',
@@ -7,6 +9,26 @@ import { Item } from '../../services/type';
   styleUrls: ['./admin-user-abc.component.css']
 })
 export class AdminUserAbcComponent implements OnInit {
+
+    id_usuario?: string = '';
+    id_empleado: string = '';
+    id_rol: string = '';
+    usuario: string = '';
+    password: string = '';
+    estatus: string = '';
+
+    response: response | any; 
+    isChecked = true;    
+    dataUserShow: user | any; 
+    enableid : boolean = false; 
+    butonAddUpdate : string = ''; 
+
+    arrayUser: user[] = [];
+    itemsTable : request_table[]=[]; 
+
+    namecolum: string[] = ['ID','Usuario','Estatus','Botones'];
+    ItemSend: String = "";
+
 
 
   itemsArray: Item[] = [
@@ -16,6 +38,7 @@ export class AdminUserAbcComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    
   }
 
 }

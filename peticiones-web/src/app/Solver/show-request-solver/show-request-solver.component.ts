29ac230ba37d111b03fa417a2d31c6fd29e25c58
SauloRLM <saulo2000ramon@gmail.com>
2022-12-10 12:03:12 -0700

    fix article  and article by problem

--- peticiones-web/src/app/Solver/show-request-solver/show-request-solver.component.ts
+++ peticiones-web/src/app/Solver/show-request-solver/show-request-solver.component.ts
@@ -2,35 +2,15 @@ import { Component, OnInit, ViewChild} from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AdminService } from 'src/app/Admin/services/admin.service';
 import {FormBuilder, Validators} from '@angular/forms';
-import {MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition} from '@angular/material/snack-bar';
+import {MatSnackBar, MatSnackBarVerticalPosition} from '@angular/material/snack-bar';
 import {MatDialog, MatDialogRef} from '@angular/material/dialog';
 import { DialogDeleteComponent } from 'src/app/components/dialog-delete/dialog-delete.component';
 import { DialogDetailComponent } from 'src/app/components/dialog-detail/dialog-detail.component';
-import { User } from 'src/app/Admin/services/type';
+import { problem, response, table_show, User } from 'src/app/Admin/services/type';
 import { MatTableDataSource } from '@angular/material/table';
 
 import {MatSort} from '@angular/material/sort';
 import {MatPaginator} from '@angular/material/paginator';
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
 
 @Component({
   selector: 'app-show-request-solver',
@@ -39,28 +19,20 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export class ShowRequestSolverComponent implements OnInit {
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
-  
-  @ViewChild(MatSort, {static: true}) sort!: MatSort;
-  @ViewChild(MatPaginator, {static: true}) paginator!: MatPaginator;
-
-  applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-  }
-
-
-
-
-
+  //contenido de tabla Pendientes
+  ItemsTableSlopes : table_show []=[];   
+  //array principal 
+  arrayProblems: problem [] = [] ;
+  response: response | any; //subscripcion de respuesta
+  dataShowProblem : problem | any;  
+  //nombres de las columas de la tabla
+  nameColums: string[] = ['Tipo de Problema','Sucursal','Fecha Registro', 'Estatus','Botones'];  
   verticalPosition: MatSnackBarVerticalPosition = 'top'; 
+
   constructor(public dialog: MatDialog ,private router: Router, private APIPetition: AdminService, private _formBuilder: FormBuilder, private _snackBar: MatSnackBar,) { }
 
   idRol : number = 0;
-  dataSesion: User|any;
-
-  
+  dataSesion: User|any;  
   ngOnInit(): void {
     if (localStorage){    
       if(localStorage.getItem('dataSesion') !== undefined && localStorage.getItem('dataSesion')){        
@@ -80,10 +52,5 @@ export class ShowRequestSolverComponent implements OnInit {
           this.router.navigate(["login"]);              
       }
     }        
-
-    this.dataSource.sort = this.sort;
-    this.dataSource.paginator = this.paginator;
-
   }
-
 }
